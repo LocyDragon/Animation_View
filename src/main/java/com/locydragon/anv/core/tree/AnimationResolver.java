b1905bf525e68520a8edf9beee67db8b8adb56a1
LocyDragon <2424441676@qq.com>
2018-06-02 09:18:39 +0800

    write code #20 rewrite pom.xml

--- src/main/java/com/locydragon/anv/core/tree/AnimationResolver.java
+++ src/main/java/com/locydragon/anv/core/tree/AnimationResolver.java
@@ -6,54 +6,56 @@ import com.locydragon.anv.api.util.AnimationJob;
 import com.locydragon.anv.api.util.AnimationObject;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+
 import java.util.Vector;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class AnimationResolver {
-	public static Vector<String> runningPlayers = new Vector<>();
-public static ReentrantLock lock = new ReentrantLock();
-public static boolean resolveAnimation(AnimationObject object, Player who) {
-	if (object == null || who == null) {
-		throw new NullPointerException("Null args");
-	}
-	for (String user : runningPlayers) {
-		if (user.equalsIgnoreCase(who.getName())) {
-			return false;
-		}
-	}
-	Thread resolveThread = new Thread(new Runnable() {
-		@Override
-		public void run() {
-			lock.lock();
+	public static final Vector<String> runningPlayers = new Vector<>();
+	public static final ReentrantLock lock = new ReentrantLock();
 
-			runningPlayers.add(who.getName());
-			for (AnimationJob job : object.getJobList()) {
-				if (job.getJobType().equalsIgnoreCase("等待")) {
-					try {
-						Thread.sleep(Integer.valueOf(job.getJobArgs()[0]) * 1000);
-					} catch (InterruptedException exc) {
-						exc.printStackTrace();
-					} finally {
-						continue;
+	public static boolean resolveAnimation(final AnimationObject object, final Player who) {
+		if (object == null || who == null) {
+			throw new NullPointerException("Null args");
+		}
+		for (String user : runningPlayers) {
+			if (user.equalsIgnoreCase(who.getName())) {
+				return false;
+			}
+		}
+		Thread resolveThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				lock.lock();
+
+				runningPlayers.add(who.getName());
+				for (final AnimationJob job : object.getJobList()) {
+					if (job.getJobType().equalsIgnoreCase("等待")) {
+						try {
+							Thread.sleep(Integer.valueOf(job.getJobArgs()[0]) * 1000);
+						} catch (InterruptedException exc) {
+							exc.printStackTrace();
+						} finally {
+							continue;
+						}
+					}
+					Bukkit.getScheduler().runTask(AnimationView.getInstance(), new Runnable() {
+						@Override
+						public void run() {
+							Bukkit.getPluginManager().callEvent(new AnimationResolveEvent(who, object, job));
+						}
+					});
+				}
+				for (int i = 0; i < runningPlayers.size(); i++) {
+					if (runningPlayers.get(i).equalsIgnoreCase(who.getName())) {
+						runningPlayers.remove(i);
 					}
 				}
-				Bukkit.getScheduler().runTask(AnimationView.getInstance(), new Runnable() {
-					@Override
-					public void run() {
-						Bukkit.getPluginManager().callEvent(new AnimationResolveEvent(who, object, job));
-					}
-				});
-			}
-			for (int i = 0;i < runningPlayers.size();i++) {
-				if (runningPlayers.get(i).equalsIgnoreCase(who.getName())) {
-					runningPlayers.remove(i);
-				}
-			}
 
-			lock.unlock();
-		}
-	});
-	resolveThread.start();
-	return true;
-}
+				lock.unlock();
+			}
+		});
+		resolveThread.start();
+		return true;
+	}
 }
