91ac0a39c7724bb9a5567737547c8e30505674c3
LocyDragon <2424441676@qq.com>
2018-06-03 20:46:45 +0800

    write code #22

--- src/main/java/com/locydragon/anv/AnimationView.java
+++ src/main/java/com/locydragon/anv/AnimationView.java
@@ -26,6 +26,8 @@ public class AnimationView extends JavaPlugin {
 	public static final String magicValue_ProtocolLib = "ProtocolLib";
 	public static AnimationView instance = null;
 	public static Vector<String> forbidTimePacket = new Vector<>();
+	public static String versionManager = org.bukkit.Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];;
+	public static int versionNum = -1;
 	@Override
 	public void onEnable() {
 		if (Bukkit.getPluginManager().getPlugin(magicValue_ProtocolLib) == null) {
@@ -41,6 +43,23 @@ public class AnimationView extends JavaPlugin {
 		Bukkit.getLogger().info("作者:LocyDragon QQ:2424441676");
 		registerEvents();
 		Bukkit.getLogger().info("AnimationView: 事件注册完成");
+		StringBuilder builder = new StringBuilder();
+		for (char c : versionManager.toCharArray()) {
+			if (Character.isDigit(c)) {
+				builder.append(c);
+			}
+		}
+		try {
+			versionNum = Integer.valueOf(builder.toString());
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		if (versionNum == -1) {
+			Bukkit.getLogger().info("加载时遇到了严重的错误.插件已经关闭....");
+			Bukkit.getPluginManager().disablePlugin(this);
+		}
+		Bukkit.getLogger().info("服务器版本: "+this.versionManager);
+		Bukkit.getLogger().info("数形版本解析: "+versionNum);
 	}
 	public static AnimationView getInstance() {
 		return instance;
---
+++ src/main/java/com/locydragon/anv/core/protocol/entity/FakeEntity.java
@@ -0,0 +1,90 @@
+package com.locydragon.anv.core.protocol.entity;
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.wrappers.WrappedDataWatcher;
+import com.google.common.base.Strings;
+import com.locydragon.anv.AnimationView;
+import org.apache.commons.lang.Validate;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+public class FakeEntity {
+	static ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
+	static AtomicInteger nextEntityID = new AtomicInteger(Integer.MAX_VALUE);
+    static void sendMovePacket(Player player, int entityId, Location current, Location prev, float yaw, float angle) {
+		Validate.notNull(current);
+		Validate.notNull(prev);
+		Validate.notNull(player);
+		PacketContainer container = protocolManager.createPacket(PacketType.Play.Server.REL_ENTITY_MOVE_LOOK);
+		container.getIntegers().write(0, entityId);
+		if (AnimationView.versionNum >= 190) {
+			container.getShorts().write(0, (short) ((current.getBlockX() * 32 - prev.getBlockX() * 32) * 128));
+			container.getShorts().write(1, (short) ((current.getBlockY() * 32 - prev.getBlockY() * 32) * 128));
+			container.getShorts().write(2, (short) ((current.getBlockZ() * 32 - prev.getBlockZ() * 32) * 128));
+			container.getBytes().write(0, (byte) yaw);
+			container.getBytes().write(1, (byte) angle);
+		} else {
+			container.getBytes().write(0, (byte) ((current.getBlockX() * 32 - prev.getBlockX() * 32) * 128));
+			container.getBytes().write(1, (byte) ((current.getBlockY() * 32 - prev.getBlockY() * 32) * 128));
+			container.getBytes().write(2, (byte) ((current.getBlockZ() * 32 - prev.getBlockZ() * 32) * 128));
+			container.getBytes().write(3, (byte) yaw);
+			container.getBytes().write(4, (byte) angle);
+		}
+		container.getBooleans().write(0, true);
+		try {
+			protocolManager.sendServerPacket(player, container);
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+	@Deprecated
+	static void sendFakeItemEntity(Player player, ItemStack itemStack, Location location, String displayName){
+		Validate.notNull(player);
+		Validate.notNull(itemStack);
+		Validate.notNull(location);
+		int entityID = nextEntityID.getAndDecrement();
+		PacketContainer spawnEntityLiving = protocolManager.createPacket(PacketType.Play.Server.SPAWN_ENTITY_LIVING);
+		spawnEntityLiving.getIntegers().write(0, entityID); //Entity ID
+		if (AnimationView.versionNum >= 190) {
+			spawnEntityLiving.getUUIDs().write(0, UUID.randomUUID()); //Entity UUID
+			spawnEntityLiving.getIntegers().write(1, 2); //Entity Type
+		} else {
+			spawnEntityLiving.getIntegers().write(1, UUID.randomUUID().variant()); //Entity UUID
+			spawnEntityLiving.getIntegers().write(2, 2); //Entity Type
+		}
+		spawnEntityLiving.getDoubles().write(0, location.getX())
+				.write(1, location.getY())
+				.write(2, location.getZ());
+		spawnEntityLiving.getIntegers().write(2, 0) //Pitch
+				.write(3, 0)	//Yaw
+				//Data
+				.write(4, 1)
+				//Velocity(X,Y,Z)
+				.write(5, 0)
+				.write(6, 0)
+				.write(7, 0);
+
+		PacketContainer entityMetadata = protocolManager.createPacket(PacketType.Play.Server.ENTITY_METADATA);
+		entityMetadata.getIntegers().write(0, entityID);
+		WrappedDataWatcher dataWatcher = new WrappedDataWatcher();
+		dataWatcher.setObject(0, (byte) 0);
+		dataWatcher.setObject(1, 300);
+		dataWatcher.setObject(2, Strings.nullToEmpty(displayName));
+		dataWatcher.setObject(3, !Strings.isNullOrEmpty(displayName));
+		dataWatcher.setObject(4, false);
+		dataWatcher.setObject(5, true);
+		dataWatcher.setObject(6, itemStack);
+		entityMetadata.getWatchableCollectionModifier().write(0, dataWatcher.getWatchableObjects());
+		try {
+			protocolManager.sendServerPacket(player, spawnEntityLiving);
+			protocolManager.sendServerPacket(player, entityMetadata);
+		} catch (InvocationTargetException e) {
+		}
+	}
+}
