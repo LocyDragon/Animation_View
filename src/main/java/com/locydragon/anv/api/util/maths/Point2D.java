96f32bec539df142de065b08d5555383b15abc3d
LocyDragon <2424441676@qq.com>
2018-06-12 20:59:14 +0800

    write code #24 add Point2D Class

--- src/main/java/com/locydragon/anv/api/util/maths/Point2D.java
+++ src/main/java/com/locydragon/anv/api/util/maths/Point2D.java
@@ -18,31 +18,50 @@ public class Point2D {
 			return returnType;
 		} else {
 			if (Math.abs(location1.getBlockX() - location2.getBlockX())
-					> Math.abs(location1.getBlockY() - location2.getBlockY())) {
+					> Math.abs(location1.getBlockZ() - location2.getBlockZ())) {
 				if (location1.getBlockX() > location2.getBlockX()) {
 					for (int i = location2.getBlockX();i < location1.getBlockX();i++) {
-						int y = x_point2D(location2.getBlockX(), location1.getBlockX()
+						double y = x_point2D(location2.getBlockX(), location1.getBlockX()
 								, location2.getBlockY(), location1.getBlockY(), i);
-						Location newLocation = new Location(location1.getWorld(), i, location1.getY(), y);
+						Location newLocation = new Location(location1.getWorld(), (double)i, location1.getBlockY(), y);
 						returnType.add(newLocation);
-						//change into double
+						return returnType;
 					}
 				} else {
-
+					for (int i = location1.getBlockX();i < location2.getBlockX();i++) {
+						double y = x_point2D(location2.getBlockX(), location1.getBlockX()
+								, location2.getBlockY(), location1.getBlockY(), i);
+						Location newLocation = new Location(location1.getWorld(), (double)i, location1.getBlockY(), y);
+						returnType.add(newLocation);
+						return returnType;
+					}
 				}
 			} else {
-				if (location1.getBlockY() > location2.getBlockY()) {
-
+				if (location1.getBlockZ() > location2.getBlockZ()) {
+					for (int z = location2.getBlockZ();z < location1.getBlockZ();z++) {
+						double x = y_point2D(location2.getBlockX(), location1.getBlockX()
+								, location2.getBlockY(), location1.getBlockY(),z);
+						Location newLocation = new Location(location1.getWorld(), x, location1.getBlockY(), (double)z);
+						returnType.add(newLocation);
+						return returnType;
+					}
 				} else {
-
+					for (int z = location1.getBlockZ();z < location2.getBlockZ();z++) {
+						double x = y_point2D(location2.getBlockX(), location1.getBlockX()
+								, location2.getBlockY(), location1.getBlockY(),z);
+						Location newLocation = new Location(location1.getWorld(), x, location1.getBlockY(), (double)z);
+						returnType.add(newLocation);
+						return returnType;
+					}
 				}
 			}
 		}
+		return returnType;
 	}
-	public static int x_point2D(int x1, int x2, int y1, int y2, int x) {
+	public static double x_point2D(int x1, int x2, int y1, int y2, int x) {
 		return (x - x2)/(x1 - x2)*(y1 - y2)+y2;
 	}
-	public static int y_point2D(int x1, int x2, int y1, int y2, int y) {
+	public static double y_point2D(int x1, int x2, int y1, int y2, int y) {
 		return (y - y2)/(y1 - y2)*(x1 - x2)+x2;
 	}
 }
